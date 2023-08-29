dcbe64367b5a66e94291c3ce618168d1bd690437
Federico Gomez <fedemgp@gmail.com>
2023-08-29 20:24:35 -0300

    Define AppConfig dataclass and add function that loads the config
    The function will read the yaml file passed, validate it using the schema configuration and then transformed into a dataclass using dacite

---
+++ pyspark_data_mocker/config/__init__.py
@@ -0,0 +1,20 @@
+import yaml
+from dacite import from_dict
+
+from .app_config import AppConfig
+from .schema import config_schema
+
+
+def get_config_from_dir(filepath: str) -> AppConfig:
+    """
+    Reads the configuration of the yaml file located in <filepath>, validates it and return an
+    AppConfig dataclass instance of the app configuration.
+
+    :param filepath:
+    :return:
+    """
+    with open(filepath, "r") as file:
+        config = yaml.safe_load(file)
+
+    config = config_schema.validate(config)
+    return from_dict(data_class=AppConfig, data=config)
---
+++ tests/config/test_get_config_from_dir.py
@@ -0,0 +1,28 @@
+import os
+import tempfile
+
+from pyspark_data_mocker.config import AppConfig, get_config_from_dir
+
+
+def test_get_config_returns_a_dataclass_fully_configured(data_dir):
+    config_path = os.path.join(data_dir, "config", "example.yaml")
+    config = get_config_from_dir(config_path)
+
+    assert isinstance(config, AppConfig)
+    assert isinstance(config.warehouse_dir, str)
+    assert config.app_name == "test"
+    assert config.warehouse_dir == "/tmp/foo/bar"
+    assert config.number_of_cores == 1
+    assert config.enable_hive
+    assert config.delta_configuration.scala_version == "2.12"
+    assert config.delta_configuration.delta_version == "2.0.2"
+    assert config.delta_configuration.snapshot_partitions == 2
+    assert config.delta_configuration.log_cache_size == 3
+
+
+def test_app_config_has_a_temporary_directory_by_default(data_dir):
+    config_path = os.path.join(data_dir, "config", "default_warehouse_temp_dir.yaml")
+    config = get_config_from_dir(config_path)
+
+    assert isinstance(config, AppConfig)
+    assert isinstance(config.warehouse_dir, tempfile.TemporaryDirectory)
